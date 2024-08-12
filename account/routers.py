e7550e61fec71b4b722eeb3da2e2eb70aefaaa8c
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-08-12 08:15:47 +0300

    create new serializer, viewset and router to handle user registration

--- account/routers.py
+++ account/routers.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
-from .viewsets import UserViewSet
+from .viewsets import UserViewSet, RegisterViewSet
 
 router = routers.SimpleRouter()
 router.register(r'users', UserViewSet, basename='user')
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 
 urlpatterns = [
     *router.urls
--- account/serializer.py
+++ account/serializer.py
@@ -10,3 +10,18 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'bio', 'email', 'is_active', 'created', 'updated', 'website_url', 'twitter_url', 'github_url', 'linkedin_url','avatar']
         read_only_field = ['is_active']
+
+
+class RegisterSerializer(UserSerializer):
+    '''
+    Serializer for registering a new user.
+    '''
+    password = serializers.CharField(max_length=128, min_length=8, write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'bio', 'email', 'website_url', 'twitter_url', 'github_url', 'linkedin_url','avatar', 'password']
+
+    
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
