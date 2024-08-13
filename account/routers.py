81552a1b335a92b1c7fc3ccaec5625f1b631ea06
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-08-13 08:16:32 +0300

    logout route

--- account/routers.py
+++ account/routers.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
-from .viewsets import LoginViewSet, UserViewSet, RegisterViewSet, RefreshViewSet
+from .viewsets import LoginViewSet, UserViewSet, RegisterViewSet, RefreshViewSet, LogoutViewSet
 
 router = routers.SimpleRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+router.register(r"auth/logout", LogoutViewSet, basename="auth-logout")
 
 urlpatterns = [
     *router.urls
