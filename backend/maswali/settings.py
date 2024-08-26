0e9d99f4aa0ce4e2fdb7b862b358dcb09e87521b
Nickson K Rop <84801830+nicky-ops@users.noreply.github.com>
2024-08-27 01:54:56 +0300

    Update settings.py

--- backend/maswali/settings.py
+++ backend/maswali/settings.py
@@ -33,7 +33,8 @@ CORS_ALLOWED_ORIGINS =  [
 		    "http://127.0.0.1:5173",
 		    "http://localhost:5173",
 	"https://maswali.vercel.app/",
-	"https://maswali-bowiqsd2s-nicky-ops-projects.vercel.app/"
+	"https://maswali-bowiqsd2s-nicky-ops-projects.vercel.app/",
+	"*"
 ]
 
 
