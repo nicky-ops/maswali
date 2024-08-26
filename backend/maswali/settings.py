18b2924f4b8b0aad9a01f6e40f99b5dd4f744cbc
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-08-27 02:19:40 +0300

    settings

--- backend/maswali/settings.py
+++ backend/maswali/settings.py
@@ -33,7 +33,8 @@ CORS_ALLOWED_ORIGINS =  [
 		    "http://127.0.0.1:5173",
 		    "http://localhost:5173",
             "https://vercel.live/link/maswali-git-main-nicky-ops-projects.vercel.app?via=deployment-domains-list-branch",
-            "https://vercel.live/link/maswali-htvh599qn-nicky-ops-projects.vercel.app?via=deployment-domains-list-commit"
+            "https://vercel.live/link/maswali-htvh599qn-nicky-ops-projects.vercel.app?via=deployment-domains-list-commit",
+            "https://maswali-htvh599qn-nicky-ops-projects.vercel.app/"
 ]
 
 
