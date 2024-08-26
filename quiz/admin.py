6f8c780e9f1acf41c1aaadfb11414cf8114205e4
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-08-26 15:45:43 +0300

    move django project to backend folder

--- quiz/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Quiz, Question, Choice, QuizAttempt
-
-admin.site.register(Category)
-admin.site.register(Quiz)
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(QuizAttempt)
