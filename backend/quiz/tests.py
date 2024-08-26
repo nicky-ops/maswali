fa4c5996d853e4c029341d821c0ad1b1312ed461
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-08-26 19:23:11 +0300

    unit test for user and quiz models

--- backend/quiz/tests.py
+++ backend/quiz/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import pytest
+from .models import Quiz, Category
+from account.fixtures.user import user
 
 # Create your tests here.
+
+@pytest.mark.django_db
+def test_create_quiz(user):
+    category = Category.objects.create(name="Python")
+    quiz = Quiz.objects.create(created_by=user, title="Quiz title", time_limit=3600, category_id=category.id)
+    assert quiz.title == "Quiz title"
+    assert quiz.time_limit == 3600
+    assert quiz.created_by == user
+    assert quiz.category_id == category.id
